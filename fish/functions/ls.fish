f0d3ef2b225b420d5708619bcbae4739bd6506a3
roshan <roshan.nair.007@gmail.com>
2023-03-09 18:56:06 +0530

    Update .gitignore, alacritty.yml, and 259 more files...

--- fish/functions/ls.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 1
-function ls --wraps='exa --header --long --group-directories-first' --description 'alias ls exa --header --long --group-directories-first'
-  exa --header --long --group-directories-first $argv;
-end
