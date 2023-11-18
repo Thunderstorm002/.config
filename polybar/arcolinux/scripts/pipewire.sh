38309ce2b0ce4bfd98ad244fdc18473a86fed7e6
roshan <roshan.nair.007@gmail.com>
2023-11-18 18:08:59 +0530

    workaround for ant being a pile of fail

--- polybar/arcolinux/scripts/pipewire.sh
+++ polybar/arcolinux/scripts/pipewire.sh
@@ -1,20 +1,17 @@
 #!/usr/bin/env sh
 # Source: https://github.com/claudius-kienle/polybar-pipewire-control/blob/master/pipewire-control.bash
 
-#getCurrSinkName() {
-  #if ! pactl info &> /dev/null; then return 1; fi
-#    curSinkName=$(pactl info | awk -F : '/Default Sink:/{print $2}')
-#}
-
-# Saves the currently default sink into a variable named `curSink`. It will
-# return an error code when pulseaudio isn't running.
+# Saves the currently default sink into a variable named `curSink`.
+# It will return an error code when pulseaudio isn't running.
 getCurSink() {
     curSinkName=$(pactl info | awk -F : '/Default Sink:/{print $2}')
-    curSink=$(pactl list sinks short | grep "$curSinkName" | awk '{print $1}')
+    #curSink=$(pactl list sinks short | grep "bluez_output.AC_80_0A_6F_44_47.1" | awk '{print $1}')
+    curSink=$(pactl list sinks short | grep $curSinkName | awk '{print $1}')
 }
 
 getCurVol() {
     getCurSink
+    #echo $curSink
     VOL_LEVEL=$(pactl list sinks | grep -A 15 'Sink #'"$curSink"'' | grep 'Volume:' | grep -E -v 'Base Volume:' | awk -F : '{print $3; exit}' | grep -o -P '.{0,3}%' | sed 's/.$//' | tr -d ' ')
 }
 
